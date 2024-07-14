f20ac3022c37aeef9cf84c0e58822180188cd3c8
rahul09999 <rahullalwani102002@gmail.com>
2024-07-15 00:50:31 +0530

    SignUp page bhul gaya bhai

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require('express')
 const path = require("path");
+const userRoute = require("./routes/user")
+
 const app = express();
 const PORT = 8000;
 
 app.set("view engine", "ejs")
 app.set('views', path.resolve('./views'));
 
+app.use('/user', userRoute);
 app.get('/', (req, res)=> {
     res.render('home')
 })
+
 
 app.listen(PORT, ()=> console.log(`Server is running or port: ${PORT}`));
---
+++ routes/user.js
@@ -0,0 +1,26 @@
+const { Router } = require("express")
+const User = require('../models/user')
+
+const router = Router();
+
+router.get("/signin", (req, res) => {
+    return res.render("signin");
+});
+
+router.get("/signup", (req, res) => {
+    return res.render("signup");
+});
+
+router.get("/signup", async (req, res) => {
+    const {fullName, email, password } = req.body;
+
+    await User.create({
+        fullName,
+        email,
+        password,
+    })
+
+    return res.redirect("/")
+});
+
+module.exports = router;
